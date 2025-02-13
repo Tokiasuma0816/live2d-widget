7660b03943ec7b90d8065b862c09c72e25741cbd
oivio-up <c15184348630@163.com>
2025-02-13 23:42:33 +0800

    更新到 1.3.3 版本,指定mita模型路径

--- dist/autoload.js
+++ dist/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.3.2/dist/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.3.3/dist/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -68,7 +68,7 @@ if (screen.width >= 768) {
         initWidget({
             waifuPath: live2d_path + "waifu-tips.json",
             //apiPath: "https://live2d.fghrsh.net/api/",
-            cdnPath: "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.3.2/",
+            cdnPath: "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.3.3/",
             modelName: "小米塔",
             tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
         });
