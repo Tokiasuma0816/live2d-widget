17aa1fb574d46fe96dda0e80a9af413533222fe9
oivio-up <c15184348630@163.com>
2025-02-26 18:25:47 +0800

    更新到 1.4.5 版本，live2d链接1.0.2

--- dist/autoload.js
+++ dist/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.4.4/dist/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/oivio-up/live2d-widget@1.4.5/dist/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -189,7 +189,7 @@ if (screen.width >= 768) {
         initWidget({
             waifuPath: live2d_path + "waifu-tips.json",
             //apiPath: "https://live2d.fghrsh.net/api/",https://cdn.jsdelivr.net/gh/oivio-up/live2d_api/https://fastly.jsdelivr.net/gh/oivio-up/live2d_api/
-            cdnPath: "https://fastly.jsdelivr.net/gh/oivio-up/live2d_api@v1.0.1/",
+            cdnPath: "https://fastly.jsdelivr.net/gh/oivio-up/live2d_api@v1.0.2/",
             tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "settings", "quit"]
         });
 
